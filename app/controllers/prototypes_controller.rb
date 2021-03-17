94976ed86789abdc787003a8db8cd764b4ca5653
aloha0055 <76520202+aloha0055@users.noreply.github.com>
2021-03-17 19:51:54 +0900

    編集機能への遷移の修正２

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: :edit
+  # before_action :contributor_confirmation, only: :edit
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -53,8 +53,8 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def contributor_confirmation
-    redirect_to root_path unless current_user == @prototype.user
-  end
+  # def contributor_confirmation
+  #   redirect_to root_path unless current_user == @prototype.user
+  # end
 end
 
